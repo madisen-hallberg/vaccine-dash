ac979db39d79aa04d25411c4f1bad248a750f9b5
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 01:20:58 -0700

    reorganized project struture to be feature based

--- vaccine-dash/src/app/store.js
+++ vaccine-dash/src/app/store.js
@@ -1,11 +1,13 @@
 // import { createStore } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
 import regionReducer from '../features/regionSelector/regionSlice'
+import vaccineReducer from '../features/vaccines/vaccineSlice'
 
 export default configureStore(
     {
         reducer: {
-            region: regionReducer
+            region: regionReducer,
+            vaccines: vaccineReducer
         }
     },
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
--- vaccine-dash/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import regionReducer from './region'
-import vaccineReducer from './vaccine'
-
-export default combineReducers({
-    region: regionReducer,
-    vaccine: vaccineReducer
-})
