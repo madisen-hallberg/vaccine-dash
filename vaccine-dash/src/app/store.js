26a87185c62d4ce08a40b01652f4734ead562a70
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 00:54:39 -0700

    setup redux dev tools

--- vaccine-dash/src/app/store.js
+++ vaccine-dash/src/app/store.js
@@ -3,6 +3,7 @@ import rootReducer from '../redux/reducers'
 
 const store = createStore(
     rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
 export default store
