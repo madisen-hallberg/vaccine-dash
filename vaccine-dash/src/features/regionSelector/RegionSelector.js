490c09137ada72306e62c70ef84aec725703e2b5
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 02:16:07 -0700

    refactor RegionSelector to use redux

--- vaccine-dash/src/features/regionSelector/RegionSelector.js
+++ vaccine-dash/src/features/regionSelector/RegionSelector.js
@@ -3,17 +3,25 @@ import Select from '@material-ui/core/Select'
 import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { setRegion, selectRegion } from './regionSlice'
+
 const regions = [ "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MH", "FM", "MP", "PW", "PR", "VI" ]
 
-export default function RegionSelector({ value, setActiveRegion }) {
+export default function RegionSelector() {
+    const region = useSelector(selectRegion)
+    const dispatch = useDispatch()
+
+    console.log(region)
+
     return (
         <FormControl>
             <InputLabel id='region-select-label'>State</InputLabel>
             <Select
                 labelId='region-select-label'
                 id='region-select'
-                value={value}
-                onChange={e => setActiveRegion(e.target.value)}
+                value={region.code}
+                onChange={e => dispatch(setRegion(e.target.value))}
             >
                 {regions.map(r => <MenuItem value={r}>{r}</MenuItem>)}
             </Select>
