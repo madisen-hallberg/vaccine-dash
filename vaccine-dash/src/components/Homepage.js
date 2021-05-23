56eadddd7abf3f3be589d0c1b16ba580979b6eda
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 05:17:41 -0700

    Cleanup old state management stuff from Home component

--- vaccine-dash/src/components/Homepage.js
+++ vaccine-dash/src/components/Homepage.js
@@ -5,21 +5,10 @@ import Vaccines from '../features/vaccines/Vaccines';
 
 
 function Home() {
-    let [vaccines, setVaccines] = useState([])
-
-    const API_KEY = process.env.REACT_APP_COVIDACTNOW_API_KEY
-    const covidactnow_url  = `https://api.covidactnow.org/v2/states.json?apiKey=${API_KEY}`
-
-    useEffect(() => {
-        fetch(covidactnow_url)
-            .then(res => res.json())
-            .then(data => setVaccines(data));
-    }, [])
-
     return(
         <div className="Homepage">
             <RegionSelector />
-            <Vaccines data={vaccines} />
+            <Vaccines />
         </div>
     );
 }
