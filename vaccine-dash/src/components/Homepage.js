658b132cee29c0e0b80f0066c51404eca7593191
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 01:31:22 -0700

    move region selector to own file & import to Home

--- vaccine-dash/src/components/Homepage.js
+++ vaccine-dash/src/components/Homepage.js
@@ -1,12 +1,8 @@
 import { useEffect, useState } from 'react'
-import InputLabel from '@material-ui/core/InputLabel'
-import Select from '@material-ui/core/Select'
-import MenuItem from '@material-ui/core/MenuItem'
-import FormControl from '@material-ui/core/FormControl'
 
+import RegionSelector from '../features/regionSelector/RegionSelector'
 import Vaccines from '../features/vaccines/Vaccines';
 
-const regions = [ "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "AS", "GU", "MH", "FM", "MP", "PW", "PR", "VI" ]
 
 function Home() {
     let [vaccines, setVaccines] = useState([])
@@ -23,18 +19,10 @@ function Home() {
 
     return(
         <div className="Homepage">
-            <FormControl>
-                <InputLabel id='region-select-label'>Choose State</InputLabel>
-                <Select
-                    labelId='region-select-label'
-                    id='region-select'
-                    value={activeRegion}
-                    onChange={e => setActiveRegion(e.target.value)}
-                >
-                    {regions.map(r => <MenuItem value={r}>{r}</MenuItem>)}
-                </Select>
-                <Vaccines data={vaccines} region={activeRegion} />
-            </FormControl>
+            <RegionSelector
+                value={activeRegion}
+                setActiveRegion={setActiveRegion} />
+            <Vaccines data={vaccines} region={activeRegion} />
         </div>
     );
 }
