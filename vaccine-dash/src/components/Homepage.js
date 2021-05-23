ad40e9e58a9c2d53580233b7109ccb25287ebaf9
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-23 16:41:24 -0700

    add historic component to homepage

--- vaccine-dash/src/components/Homepage.js
+++ vaccine-dash/src/components/Homepage.js
@@ -1,11 +1,13 @@
 import RegionSelector from '../features/regionSelector/RegionSelector'
 import Vaccines from '../features/vaccines/Vaccines';
+import Historic from '../features/historicDisplay/HistoricDisplay'
 
 function Home() {
     return(
         <div className="Homepage">
             <RegionSelector />
             <Vaccines />
+            <Historic />
         </div>
     );
 }
