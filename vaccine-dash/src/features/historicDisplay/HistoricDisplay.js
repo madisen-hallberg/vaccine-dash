ab6d32aa95c68703c337b25840792bd6d5974a09
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-24 22:07:45 -0700

    add loading spinner to historic data

--- vaccine-dash/src/features/historicDisplay/HistoricDisplay.js
+++ vaccine-dash/src/features/historicDisplay/HistoricDisplay.js
@@ -1,28 +1,32 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { activeRegionCode } from '../regionSelector/regionSlice'
-import { fetchHistoricData, timeSeriesData, riskLevelsTimeseries } from './historicSlice'
+import { fetchHistoricData } from './historicSlice'
+
+import CircularProgress from '@material-ui/core/CircularProgress'
 
 // TODO: filter by activeRegion
 export default function HistoricDisplay() {
     const dispatch = useDispatch()
     const loadStatus = useSelector(state => state.historic.status)
     const regionCode = useSelector(activeRegionCode)
-    const riskLevels = useSelector(riskLevelsTimeseries)
+    const riskLevels = useSelector(state => state.historic.data.riskLevelsTimeseries)
 
     useEffect(() => {
         if (loadStatus === 'pending') {
             dispatch(fetchHistoricData(regionCode))
         }
     }, [loadStatus, dispatch, regionCode])
-
-    console.log('Risk level timeseries', riskLevels)
-    const dates = riskLevels.map(d => new Date(d.date))
-    const overalRiskLevel = riskLevels .map(d => d.overall)
+    
+    console.log(riskLevels)
+    // const dates = riskLevels.map(d => new Date(d.date))
+    // const overalRiskLevel = riskLevels.map(d => d.overall)
 
     return (
         <section>
             <h2>Vaccine Over Time</h2>
+            {loadStatus !== 'succeeded' && <CircularProgress />}
+            {loadStatus === 'succeeded' && <p>Data Loaded</p>}
         </section>
     )
 }
