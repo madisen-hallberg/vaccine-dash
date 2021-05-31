6b00e99e37a13b935f497ff45c88e72b48b6f0ea
Geoff Corvera <geoffcorvera@gmail.com>
2021-05-31 00:49:38 -0700

    move line chart into separate file

--- vaccine-dash/src/features/historicDisplay/HistoricDisplay.js
+++ vaccine-dash/src/features/historicDisplay/HistoricDisplay.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from 'react-redux'
 import { activeRegionCode } from '../regionSelector/regionSlice'
 import { fetchHistoricData, allLineChartData } from './historicSlice'
 
-import { ResponsiveLine } from '@nivo/line'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import LineChart from '../../components/LineChart'
 
 // TODO: filter by activeRegion
 export default function HistoricDisplay() {
@@ -36,77 +36,5 @@ export default function HistoricDisplay() {
             {loadStatus !== 'succeeded' && <CircularProgress />}
             {loadStatus === 'succeeded' && <LineChart data={lineChartData} />}
         </div>
-    )
-}
-
-// make sure parent container have a defined height when using
-// responsive component, otherwise height will be 0 and
-// no chart will be rendered.
-// website examples showcase many properties,
-// you'll often use just a few of them.
-const LineChart = ({ data }) => {
-    if (data === undefined) {
-        return <p>No Data</p>
-    }
-    return (
-        <ResponsiveLine
-            data={data}
-            margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-            xScale={{ type: 'point' }}
-            yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-            yFormat=" >-.2f"
-            axisTop={null}
-            axisRight={null}
-            axisBottom={{
-                orient: 'bottom',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'Vaccination',
-                legendOffset: 36,
-                legendPosition: 'middle'
-            }}
-            axisLeft={{
-                orient: 'left',
-                tickSize: 5,
-                tickPadding: 5,
-                tickRotation: 0,
-                legend: 'count',
-                legendOffset: -40,
-                legendPosition: 'middle'
-            }}
-            pointSize={10}
-            pointColor={{ theme: 'background' }}
-            pointBorderWidth={2}
-            pointBorderColor={{ from: 'serieColor' }}
-            pointLabelYOffset={-12}
-            useMesh={true}
-            legends={[
-                {
-                    anchor: 'bottom-right',
-                    direction: 'column',
-                    justify: false,
-                    translateX: 100,
-                    translateY: 0,
-                    itemsSpacing: 0,
-                    itemDirection: 'left-to-right',
-                    itemWidth: 80,
-                    itemHeight: 20,
-                    itemOpacity: 0.75,
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemBackground: 'rgba(0, 0, 0, .03)',
-                                itemOpacity: 1
-                            }
-                        }
-                    ]
-                }
-            ]}
-        />
     )
 }
